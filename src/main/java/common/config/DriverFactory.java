b946c71b98198ce474cc0d0f1b52fc896d878b51
Danaka <dana_catavei@yahoo.com>
2023-10-27 19:14:57 +0300

    Update framework

--- src/main/java/common/config/DriverFactory.java
+++ src/main/java/common/config/DriverFactory.java
@@ -1,4 +1,22 @@
 package common.config;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class DriverFactory {
+    public static WebDriver driver_local;
+    public static final String url = "https://www.saucedemo.com/";
+    public void setup(){
+
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY, "true");
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver_local = new ChromeDriver(options);
+        driver_local.manage().window().maximize();
+        driver_local.get(url);
+    }
 }
